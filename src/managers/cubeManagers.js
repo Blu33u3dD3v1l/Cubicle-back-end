586b53e4c9f0b80db233145b8b21f74ef1d4f8d5
ViktoR <Kachamakov@abv.bg>
2024-01-17 17:41:43 +0200

    Add details

--- src/managers/cubeManagers.js
+++ src/managers/cubeManagers.js
@@ -1,6 +1,19 @@
-const cubes = [];
+const cubes = [
+
+{
+    id: '1nfdfdkf33gfdg55gkj',
+    name: 'Mirror Cube',
+    imageUrl: 'https://media.istockphoto.com/id/473622694/photo/rubiks-cube.jpg?s=612x612&w=0&k=20&c=jG8vgbrd8jsLPYwzNTgoALf86E20dbTBC30S77QjyhY=',
+    description: "This is the best cube!",
+    difficultyLevel: 4,
+
+
+}
+
+];
 
 exports.getAll = () => cubes.slice();
+exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
     
 exports.create = (cubeData) => {
             const newCube = {
