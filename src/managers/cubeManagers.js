ea9882459d8cd419fea10d43df45a65f33953421
ViktoR <Kachamakov@abv.bg>
2024-01-22 17:38:02 +0200

    db config

--- src/managers/cubeManagers.js
+++ src/managers/cubeManagers.js
@@ -1,16 +1,4 @@
-const cubes = [
-
-{
-    id: '1nfdfdkf33gfdg55gkj',
-    name: 'Mirror Cube',
-    imageUrl: 'https://media.istockphoto.com/id/473622694/photo/rubiks-cube.jpg?s=612x612&w=0&k=20&c=jG8vgbrd8jsLPYwzNTgoALf86E20dbTBC30S77QjyhY=',
-    description: "This is the best cube!",
-    difficultyLevel: 4,
-
-
-}
-
-];
+const cubes = [];
 
 exports.getAll = (search, from, to) => {
 
