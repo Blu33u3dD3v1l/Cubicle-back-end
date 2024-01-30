ac43e19b013003b868ce977feac4fcf8cff6243e
ViktoR <Kachamakov@abv.bg>
2024-01-30 13:19:16 +0200

    Hash password before save

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt');
+
 const userScheme = new mongoose.Schema({
-         name: String,
-         password: String,
+         username: String,
+         password: {
+            type: String,
+         },
                
 });
+
+
+userScheme.virtual('repeatPassword')
+.set(function(value){
+     if(value !== this.password){
+        throw new mongoose.MongooseError('Password missmach');
+     }
+})
+
+userScheme.pre('save', async function() {
+     const hash = await bcrypt.hash(this.password, 10);
+
+     this.password = hash;
+
+});
+
 
 const User = mongoose.model('User', userScheme);
 
